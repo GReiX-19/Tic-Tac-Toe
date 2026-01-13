2a48bf6ece63c6fb482319a637ad1cdb4c962881
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-13 13:07:16 +0200

    created class GameState in GameState.h

---
+++ modules/EngineCore/include/GameState.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Board.h"
+#include "Player.h"
+
+namespace EngineCore {
+	class GameState {
+	public:
+		
+		GameState();
+
+		bool make_move(int _row, int _col, Player _player);
+		bool is_win(Player _player) const;
+		bool is_draw() const;
+
+		Player get_current_player() const;
+		const Board& get_board() const;
+
+		void reset();
+
+	private:
+		bool check_win_for(CellState _state) const;
+	private:
+		Board m_board;
+		Player m_current_player;
+	};
+}
