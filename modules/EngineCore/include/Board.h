f3145cf975d1114400443228a67d29f4c3250faa
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-13 13:01:14 +0200

    created class Board

--- modules/EngineCore/include/Board.h
+++ modules/EngineCore/include/Board.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <array>
+#include "CellState.h"
+
+namespace EngineCore {
+	class Board {
+	public:
+		
+		static constexpr int SIZE = 3;
+
+	public:
+
+		Board();
+
+		CellState get_CellState(int row, int col) const;
+
+		bool set_CellState(int row, int col, CellState state);
+
+		void reset();
+
+	private:
+		std::array<std::array<CellState, SIZE>, SIZE> m_cells;
+	};
