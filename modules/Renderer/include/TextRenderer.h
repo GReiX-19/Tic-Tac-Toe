ed9bcf2947895682339ed103951cb929d1c8b167
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-15 13:58:47 +0200

    created TextRenderer

---
+++ modules/Renderer/include/TextRenderer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "AssetsManager.h"
+
+namespace Renderer {
+	class TextRenderer {
+	public:
+		TextRenderer(AssetsManager& _assetsManager);
+
+		void draw_centered_text(sf::RenderWindow& _window, std::string_view _text, const size_t _charSize,
+			const sf::Color& _color, const float _y);
+
+		void draw_game_over_text(sf::RenderWindow& _window);
+
+	private:
+		AssetsManager& m_assetsManager;
+	};
+}
