bd8ac4a820324bdaf3ced0e748d0175eabad3d0f
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-17 17:41:34 +0200

    drawing and handling playButton

--- modules/Renderer/source/RenderScreens/MenuScreen.cpp
+++ modules/Renderer/source/RenderScreens/MenuScreen.cpp
@@ -2,10 +2,33 @@
 
 namespace Renderer {
 	MenuScreen::MenuScreen(Renderer& _renderer, AssetsManager& _assetsManager)
-		: m_renderer(_renderer), m_assetsManager(_assetsManager), m_textRenderer(_assetsManager) {
+		: m_renderer(_renderer), m_assetsManager(_assetsManager), m_textRenderer(_assetsManager)
+		, m_playButton(_assetsManager.get_texture("playButton")) {
+		m_playButton.setOrigin(m_playButton.getGlobalBounds().getCenter());
+		m_playButton.setPosition({ 400.f, 400.f });
 	}
 
 	void MenuScreen::handle_event(const sf::Event& _event) {
+		if (const auto* mousePos = _event.getIf<sf::Event::MouseMoved>()) {
+			if (m_playButton.getGlobalBounds().contains(sf::Vector2f(mousePos->position)))
+				m_playButton.setTexture(m_assetsManager.get_texture("playButtonHovered"));
+		}
+
+		if (const auto* mousePos = _event.getIf<sf::Event::MouseButtonPressed>()) {
+			if (mousePos->button == sf::Mouse::Button::Left) {
+				if (m_playButton.getGlobalBounds().contains(sf::Vector2f(mousePos->position))) {
+					m_playButton.setTexture(m_assetsManager.get_texture("playButton"));
+				}
+			}
+		}
+		else if (const auto* mousePos = _event.getIf<sf::Event::MouseButtonReleased>()) {
+			if (mousePos->button == sf::Mouse::Button::Left) {
+				if (m_playButton.getGlobalBounds().contains(sf::Vector2f(mousePos->position))) {
+					m_renderer.switch_state(AppState::Game);
+				}
+			}
+		}
+
 		if (const auto* key = _event.getIf<sf::Event::KeyPressed>()) {
 			if (key->scancode == sf::Keyboard::Scan::Enter) {
 				m_renderer.switch_state(AppState::Game);
@@ -19,6 +42,6 @@ namespace Renderer {
 
 	void MenuScreen::draw(sf::RenderWindow& _window) {
 		m_textRenderer.draw_centered_text(_window, "Welcome to Tic Tac Toe Game!", 40, sf::Color::White, (_window.getSize().y / 2.0f) - 30.f);
-		m_textRenderer.draw_centered_text(_window, "Press Enter to Start", 32, sf::Color::White, (_window.getSize().y / 2.0f) + 30.f);
+		_window.draw(m_playButton);
 	}
 } 
