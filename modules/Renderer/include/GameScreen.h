bcd20dab03e3d32551fa977e9fba0b963c7ef626
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-17 12:27:00 +0200

    created GameScreen

---
+++ modules/Renderer/include/GameScreen.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "IScreen.h"
+#include "AssetsManager.h"
+#include "Renderer.h"
+
+namespace Renderer {
+	class GameScreen : public IScreen {
+	public:
+
+		GameScreen(Renderer& _renderer, AssetsManager& _assetsManager, EngineCore::GameState& state);
+
+		void handle_event(const sf::Event& _event) override;
+		void update(float _dt) override;
+		void draw(sf::RenderWindow& _window) override;
+
+	private:
+		Renderer& m_renderer;
+		AssetsManager& m_assetsManager;
+		EngineCore::GameState& m_gameState;
+
+		BoardView m_boardView;
+	};
+}
