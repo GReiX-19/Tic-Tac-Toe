2c1ff76c5fbe2f131b194809ad2eba6955f2fc15
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-17 21:52:34 +0200

    deleted determine_winner and get_winner

--- modules/EngineCore/source/GameState.cpp
+++ modules/EngineCore/source/GameState.cpp
@@ -13,55 +13,17 @@ namespace EngineCore {
 		}
 		return false;
 	}
-
 	bool GameState::is_win(Player _player) const {
 		CellState state = (_player == Player::PLAYER_X) ? CellState::X : CellState::O;
 		return check_win_for(state);
 	}
-
-	bool GameState::check_win_for(CellState _state) const {
-		// Check rows and columns
-		for (int i = 0; i < 3; ++i) {
-			if ((m_board.get_CellState(i, 0) == _state && m_board.get_CellState(i, 1) == _state && m_board.get_CellState(i, 2) == _state) ||
-				(m_board.get_CellState(0, i) == _state && m_board.get_CellState(1, i) == _state && m_board.get_CellState(2, i) == _state)) {
-				return true;
-			}
-		}
-		// Check diagonals
-		if ((m_board.get_CellState(0, 0) == _state && m_board.get_CellState(1, 1) == _state && m_board.get_CellState(2, 2) == _state) ||
-			(m_board.get_CellState(0, 2) == _state && m_board.get_CellState(1, 1) == _state && m_board.get_CellState(2, 0) == _state)) {
-			return true;
-		}
-		return false;
-	}
-	void GameState::determine_winner() {
-		if (is_win(Player::PLAYER_X)) {
-			m_winner = Player::PLAYER_X;
-		}
-		else if (is_win(Player::PLAYER_O)) {
-			m_winner = Player::PLAYER_O;
-		}
-		else {
-			m_winner = Player::PLAYER_X; // No winner
-		}
-	}
-
 	bool GameState::is_draw() const {
 		return m_board.is_full() && !is_win(Player::PLAYER_X) && !is_win(Player::PLAYER_O);
-	}
-
-	void GameState::reset() {
-		m_board.reset();
-		m_current_player = Player::PLAYER_X;
 	}
 
 	const Board& GameState::get_board() const {
 		return m_board;
 	}
-	const Player& GameState::get_winner() const {
-		return m_winner;
-	}
-
 	GameState::winInfo GameState::get_win_info() const {
 		winInfo info;
 		// Check rows
@@ -110,5 +72,26 @@ namespace EngineCore {
 			return info;
 		}
 		return info;
+	}
+
+	void GameState::reset() {
+		m_board.reset();
+		m_current_player = Player::PLAYER_X;
+	}
+
+	bool GameState::check_win_for(CellState _state) const {
+		// Check rows and columns
+		for (int i = 0; i < 3; ++i) {
+			if ((m_board.get_CellState(i, 0) == _state && m_board.get_CellState(i, 1) == _state && m_board.get_CellState(i, 2) == _state) ||
+				(m_board.get_CellState(0, i) == _state && m_board.get_CellState(1, i) == _state && m_board.get_CellState(2, i) == _state)) {
+				return true;
+			}
+		}
+		// Check diagonals
+		if ((m_board.get_CellState(0, 0) == _state && m_board.get_CellState(1, 1) == _state && m_board.get_CellState(2, 2) == _state) ||
+			(m_board.get_CellState(0, 2) == _state && m_board.get_CellState(1, 1) == _state && m_board.get_CellState(2, 0) == _state)) {
+			return true;
+		}
+		return false;
 	}
 }
