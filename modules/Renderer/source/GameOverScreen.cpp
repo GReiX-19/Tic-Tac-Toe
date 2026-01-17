22fbcb4a867165b3a2896a5a5bb02ee28236a124
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-17 14:16:32 +0200

    definitions of GameOverScreen functions

---
+++ modules/Renderer/source/GameOverScreen.cpp
@@ -0,0 +1,45 @@
+#include "GameOverScreen.h"
+
+namespace Renderer {
+	GameOverScreen::GameOverScreen(Renderer& _renderer, AssetsManager& _assetsManager, EngineCore::GameState& _state)
+		: m_renderer(_renderer)
+		, m_assetsManager(_assetsManager)
+		, m_gameState(_state)
+		, m_gameOverText(_assetsManager.get_font("mainFont"))
+		, m_restartInstructionText(_assetsManager.get_font("mainFont")) {
+		if (m_gameState.is_draw()) {
+			m_gameOverText.setString("It's a Draw!");
+		}
+		else if (m_gameState.get_current_player() == EngineCore::Player::PLAYER_X) {
+			m_gameOverText.setString("Player X Wins!");
+		}
+		else {
+			m_gameOverText.setString("Player O Wins!");
+		}
+		m_gameOverText.setCharacterSize(64);
+		m_gameOverText.setPosition({ 120.f, 100.f });
+
+		m_restartInstructionText.setString("Press Enter to Restart");
+		m_restartInstructionText.setCharacterSize(32);
+		m_restartInstructionText.setPosition({ 180.f, 300.f });
+	}
+
+	void GameOverScreen::handle_event(const sf::Event& _event) {
+		if (const auto* key = _event.getIf<sf::Event::KeyPressed>()) {
+			if (key->scancode == sf::Keyboard::Scan::Enter) {
+				m_gameState.reset();
+				m_renderer.switch_state(AppState::Game);
+			}
+		}
+	}
+
+	void GameOverScreen::update(float _dt) {
+		// No update logic needed for the game over screen
+	}
+
+	void GameOverScreen::draw(sf::RenderWindow& _window) {
+		_window.clear(sf::Color::Black);
+		_window.draw(m_gameOverText);
+		_window.draw(m_restartInstructionText);
+	}
+}
