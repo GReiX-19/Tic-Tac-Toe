7807b6168d5a2d4bbbe4b02937f9b9309ba051e6
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-17 12:01:51 +0200

    created interface class IScreen

---
+++ modules/Renderer/include/IScreen.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace Renderer {
+	class IScreen {
+	public:
+
+		virtual ~IScreen() = default;
+
+		virtual void hangle_event(const sf::Event& _event) = 0;
+		virtual void update(float& _dt) = 0;
+		virtual void draw(sf::RenderWindow& _window) = 0;
+	};
+}
