23ea5909fb764563cb6a35d146574b6a434662b6
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-20 20:57:59 +0200

    minor change

--- modules/GameApp/source/GameApp.cpp
+++ modules/GameApp/source/GameApp.cpp
@@ -1,9 +1,11 @@
 #include "GameApp.h"
 
 namespace GameApp {
-	GameApp::GameApp() : m_gameState(), m_renderer(m_gameState) {}
+
+	GameApp::GameApp() : m_gameState(), m_assetsManager(), m_renderer(m_gameState, m_assetsManager) {}
 
 	void GameApp::execute() {
 		m_renderer.run();
 	}
+
 }
