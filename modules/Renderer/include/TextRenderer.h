a598cbbe3d901b826960c160eed296caabb6cb3a
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-20 19:36:59 +0200

    decided to add just draw_text method in TextRenderer, i just need it so why not

--- modules/Renderer/include/TextRenderer.h
+++ modules/Renderer/include/TextRenderer.h
@@ -11,10 +11,14 @@ namespace Renderer {
 
 	class TextRenderer {
 	public:
+
 		TextRenderer(AssetsManager& _assetsManager);
 
+		void draw_text(sf::RenderWindow& _window, std::string_view _text, const size_t& _charSize,
+			const sf::Color& _color, const float& _x, const float& _y);
 		void draw_centered_text(sf::RenderWindow& _window, std::string_view _text, const size_t& _charSize,
 			const sf::Color& _color, const float& _y);
+
 	private:
 		AssetsManager& m_assetsManager;
 	};
--- modules/Renderer/source/TextRenderer.cpp
+++ modules/Renderer/source/TextRenderer.cpp
@@ -8,6 +8,19 @@ namespace Renderer {
 		: m_assetsManager(_assetsManager) {
 	}
 
+	void TextRenderer::draw_text(sf::RenderWindow& _window, std::string_view _text, const size_t& _charSize,
+		const sf::Color& _color, const float& _x, const float& _y) {
+		sf::Text text(m_assetsManager.get_font("mainFont"), sf::String(_text.data()), _charSize);
+		text.setFillColor(_color);
+		text.setOutlineThickness(2.f);
+		text.setOutlineColor(sf::Color::Black);
+
+		sf::FloatRect textBounds = text.getLocalBounds();
+		text.setOrigin({ textBounds.getCenter().x, textBounds.getCenter().y });
+		text.setPosition({ _x, _y });
+
+		_window.draw(text);
+	}
 	void TextRenderer::draw_centered_text(sf::RenderWindow& _window, std::string_view _text,
 		const size_t& _charSize, const sf::Color& _color, const float& _y) {
 		sf::Text text(m_assetsManager.get_font("mainFont"), sf::String(_text.data()), _charSize);
@@ -17,7 +30,7 @@ namespace Renderer {
 
 		sf::FloatRect textBounds = text.getLocalBounds();
 		text.setOrigin({ textBounds.getCenter().x, textBounds.getCenter().y });
-		text.setPosition({ _window.getSize().x / 2.f, _y });
+		text.setPosition({ (_window.getSize().x / 2.f), _y });
 
 		_window.draw(text);
 	}
