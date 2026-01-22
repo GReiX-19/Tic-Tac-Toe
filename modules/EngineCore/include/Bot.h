6276e93a75611aca2c3f85849ea90948fcd1920a
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-22 21:40:10 +0200

    created Bot

---
+++ modules/EngineCore/include/Bot.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Board.h"
+#include "CellState.h"
+
+namespace EngineCore {
+	class Bot {
+	public:
+
+		Bot(CellState _botState);
+
+		std::pair<uint16_t, uint16_t> get_move(const Board& _board);
+
+	private:
+
+		bool can_win(const Board& _board, uint16_t& _outRow, uint16_t _outCol, CellState _state);
+		bool find_blocking_move(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
+		bool find_center(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
+		bool find_corner(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
+		bool find_any(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
+
+	private:
+		CellState m_botState;
+		CellState m_playerState;
+	};
+}
