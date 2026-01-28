fbf2723beea05c309ba28ec888ed03142bdb3205
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-01-28 16:43:24 +0200

    bot can check win himself

--- modules/EngineCore/include/Bot.h
+++ modules/EngineCore/include/Bot.h
@@ -13,10 +13,11 @@ namespace EngineCore {
 	private:
 
 		bool can_win(const Board& _board, uint16_t& _outRow, uint16_t _outCol, CellState _state);
-		bool find_blocking_move(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
 		bool find_center(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
 		bool find_corner(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
 		bool find_any(const Board& _board, uint16_t& _outRow, uint16_t _outCol);
+
+		bool check_win(const Board& _board, CellState _state);
 
 	private:
 		CellState m_botState;
