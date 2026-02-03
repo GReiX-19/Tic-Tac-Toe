1391265562f1e632ef95026b469f52efa5cd9121
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-02-03 10:15:43 +0200

    bug fix

--- modules/Renderer/source/RenderScreens/GameOverScreen.cpp
+++ modules/Renderer/source/RenderScreens/GameOverScreen.cpp
@@ -67,6 +67,8 @@ namespace Renderer {
 				m_renderer.switch_state(AppState::Game);
 			}
 			if (key->scancode == sf::Keyboard::Scan::Escape) {
+				m_gameState.reset();
+				m_gameState.reset_wins();
 				m_renderer.switch_state(AppState::Menu);
 			}
 		}
