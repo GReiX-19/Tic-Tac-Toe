f68dd8f23c83c81382784e75c9c439575241b94c
GReiX-19 <GReiX-19@DESKTOP-R5LRRFV>
2026-02-08 21:21:32 +0200

    now our Bot can analize board by itself and make moves

--- modules/EngineCore/include/Bot.h
+++ modules/EngineCore/include/Bot.h
@@ -7,9 +7,18 @@ namespace EngineCore {
 
 		Bot(PlayerMark _playerStatus);
 
-		bool make_move(Board& _board);
+		void make_move(Board& _board);
 
 		PlayerMark get_status() const override;
+
+	private:
+
+		bool can_win(const Board& _board, std::pair<uint16_t, uint16_t>& _cell, const CellState& _state);
+		bool find_center(const Board& _board, std::pair<uint16_t, uint16_t>& _cell);
+		bool find_corner(const Board& _board, std::pair<uint16_t, uint16_t>& _cell);
+		bool find_any(const Board& _board, std::pair<uint16_t, uint16_t>& _cell);
+
+		bool check_win(const Board& _board, const CellState& _state);
 
 	};
 }
--- modules/EngineCore/source/Bot.cpp
+++ modules/EngineCore/source/Bot.cpp
@@ -1,20 +1,100 @@
 #include "Bot.h"
 #include "CellState.h"
 #include "Board.h"
-#include <utility>
 
 namespace EngineCore {
 	Bot::Bot(PlayerMark _playerStatus) : IPlayer(_playerStatus) {}
 
-	bool Bot::make_move(Board& _board) {
+	void Bot::make_move(Board& _board) {
 		CellState state = (get_status() == PlayerMark::PLAYER_X) ? CellState::X : CellState::O;
+		std::pair<uint16_t, uint16_t> cell;
 
-
-
-		return false;
+		if (can_win(_board, cell, state))
+			_board.set_CellState(cell, state);
+		if (can_win(_board, cell, state))
+			_board.set_CellState(cell, state);
+		if (can_win(_board, cell, state))
+			_board.set_CellState(cell, state);
+		
+		find_any(_board, cell);
+		_board.set_CellState(cell, state);
 	}
 
 	PlayerMark Bot::get_status() const {
 		return m_playerStatus;
+	}
+
+	bool Bot::can_win(const Board& _board, std::pair<uint16_t, uint16_t>& _cell, const CellState& _state) {
+		for (uint16_t row = 0; row < 3; row++) {
+			for (uint16_t col = 0; col < 3; col++) {
+				if (_board.get_CellState({ row, col }) != CellState::EMPTY)
+					continue;
+
+				Board copiedBoard = _board;
+				copiedBoard.set_CellState({ row, col }, _state);
+				if (check_win(copiedBoard, _state)) {
+					_cell.first = row;
+					_cell.second = col;
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+	bool Bot::find_center(const Board& _board, std::pair<uint16_t, uint16_t>& _cell) {
+		if (_board.get_CellState({ 1,1 }) == CellState::EMPTY) {
+			_cell.first = 1;
+			_cell.second = 1;
+			return true;
+		}
+
+		return false;
+	}
+	bool Bot::find_corner(const Board& _board, std::pair<uint16_t, uint16_t>& _cell) {
+		const std::pair<uint16_t, uint16_t> corners[4] = {
+			{0,0},
+			{0,2},
+			{2,0},
+			{2,2}
+		};
+
+		for (auto cell : corners) {
+			if (_board.get_CellState(cell) == CellState::EMPTY) {
+				_cell.first = cell.first;
+				_cell.second = cell.second;
+				return true;
+			}
+		}
+
+		return false;
+	}
+	bool Bot::find_any(const Board& _board, std::pair<uint16_t, uint16_t>& _cell) {
+		for (uint16_t row = 0; row < 3; row++) {
+			for (uint16_t col = 0; col < 3; col++) {
+				if (_board.get_CellState({row, col}) == CellState::EMPTY) {
+					_cell.first = row;
+					_cell.second = col;
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	bool Bot::check_win(const Board& _board, const CellState& _state) {
+		for (int i = 0; i < 3; ++i) {
+			if ((_board.get_CellState({ i, 0 }) == _state && _board.get_CellState({ i, 1 }) == _state && _board.get_CellState({ i, 2 }) == _state) ||
+				(_board.get_CellState({ 0, i }) == _state && _board.get_CellState({ 1, i }) == _state && _board.get_CellState({ 2, i }) == _state)) {
+				return true;
+			}
+		}
+		// Check diagonals
+		if ((_board.get_CellState({ 0, 0 }) == _state && _board.get_CellState({ 1, 1 }) == _state && _board.get_CellState({ 2, 2 }) == _state) ||
+			(_board.get_CellState({ 0, 2 }) == _state && _board.get_CellState({ 1, 1 }) == _state && _board.get_CellState({ 2, 0 }) == _state)) {
+			return true;
+		}
+		return false;
 	}
 }
